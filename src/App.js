024926e6661f7dc4d37ee7a46967ace079b56051
Marielle <marielle@theredmoon.ca>
2020-11-29 12:15:42 -0500

    axios call successful and structure created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 import './App.css';
 import axios from "axios";
+import LatestNewsTiles from './LatestNewsTiles.js';
 
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      stories: [],
+    };
+  }
+  
+  componentDidMount() {
+    const apiKey = `3JUrifoNOPNfy5hZecIGlU6pbGQ0f71x`;
+    axios({
+      method: "GET",
+      url: `https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=3JUrifoNOPNfy5hZecIGlU6pbGQ0f71x`,
+      dataResponse: "json",
+      // params: {
+      //   // apikey: apiKey
+      //   // section: technology,
+      // }
+    }).then((response) => {
+      response = response.data.results;
+      console.log(response);
+    })
+  }
+
+
+
   render() {
     return (
       <div>
